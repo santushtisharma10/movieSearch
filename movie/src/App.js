ce7e5849f21dc50afd41d05f7e477ef3310dfb65
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-02-14 14:50:29 +0530

    changes added

--- movie/src/App.js
+++ movie/src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div >
-      <div className="upper">
-        <h1>Movie Search App</h1>
-      </div>
+      
       <div ></div>
     </div>
   );
