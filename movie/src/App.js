b021f2903b1217362b524182974048ef3e3922ff
santushtisharma10 <sharmasantushti1012@gmail.com>
2021-02-15 14:24:35 +0530

    search movie files added

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import SearchMovie from "./SearchMovie";
 
 function App() {
   return (
     <div >
-      
+      <SearchMovie />
       <div ></div>
     </div>
   );
---
+++ movie/src/SearchMovie.js
@@ -0,0 +1,18 @@
+import "./SearchMovie.css"
+
+export default function SearchMovie() {
+
+    return(
+        <div>
+            <form>
+                <br />
+                <br />
+                <br />
+                <input type="query" placeholder="Movie Name"></input>
+                <button type="submit">&nbsp;&nbsp; Search &nbsp;&nbsp;</button>
+            </form>
+        </div>
+
+    )
+
+}
